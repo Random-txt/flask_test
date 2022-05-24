0dcd7a1a9b4a547cc80be50ee6e7590e8fe1b8f2
DESKTOP-QT6KML1\33767 <grrmrvtj@gmail.com>
2022-05-24 14:53:07 +0200

    fix

--- application.py
+++ application.py
@@ -2,10 +2,11 @@ from distutils.log import debug
 from flask import Flask
 
 application = Flask(__name__)
+app = application
 
 @application.route('/')
 def test_world():
     return 'test is working'
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
